695369c05f711126d5b2698d26749c3583dcba9d
Sava Petrovic <petrovicsava3@gmail.com>
2023-09-27 08:27:59 +0200

    Code cleaned up

--- src/app/services/cart.service.ts
+++ src/app/services/cart.service.ts
@@ -32,6 +32,34 @@ export class CartService {
       }
     }
   }
+  decrease(product: ProductCatalog): void {
+    if (!this.currentCart$.value.length) {
+      this.currentCart$.next([{ product, quantity: 1 }]);
+    } else {
+      const potentialProductIndex = this.currentCart$.value.findIndex(
+        (orderProduct) => orderProduct.product.id === product.id,
+      );
+
+      if (potentialProductIndex > -1) {
+        const currentCart = this.currentCart$.value;
+        currentCart[potentialProductIndex].quantity += 1;
+        this.currentCart$.next([...currentCart]);
+      } else {
+        this.currentCart$.next([
+          ...this.currentCart$.value,
+          { product, quantity: 1 },
+        ]);
+      }
+    }
+  }
+
+  remove(product: ProductCatalog): void {
+    const cartWithRemovedProduct = this.currentCart$.value.filter(
+      (orderProduct) => orderProduct.product.id !== product.id,
+    );
+
+    this.currentCart$.next(cartWithRemovedProduct);
+  }
 
   getCart$(): Observable<OrderProduct[]> {
     return this.currentCart$.asObservable();
